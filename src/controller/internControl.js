5f7ca3784e1fb10505d7ee8a3d8a35b483661963
kashi <kashisharmaujjn@gmail.com>
2023-06-11 14:21:33 +0530

    Intern-Hub-Project-Complete

---
+++ src/controller/internControl.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const collegeModel = require('../models/collegeModel');
+const internModel = require('../models/internModel');
+const { validateEmail, isValidNumber,isValid } = require('../utils/util.js')
+
+const createIntern = async function (req, res) {
+    {
+        try{
+        let input = req.body;
+        let { name, email, mobile, collegeId, isDeleted } = input;
+
+        if(!name || !email || !mobile || !collegeId ){
+        return res.status(400).send({status : false , message : "Please provide manadatory details!"})
+        }
+
+        if (!isValid(email)) { return res.status(400).send({ status: false, message: " Invalid email" }) }
+
+        if (!validateEmail(email)) { return res.status(400).send({ status: false, message: "Enter the valid email" }) }
+
+        if(!isValidNumber) return res.status(400).send({status:false,message :"Please provide correct Mobile number!"})
+
+        collegeId = await collegeModel.findOne({collegeId : collegeId});
+        if(!collegeId) return res.status(400).send({status : false, message : "Student is not registered!"})
+        
+        let createData = await internModel.create(input);
+        return res.status(201).send({status : true , data : createData});
+    }
+    catch(error) {return res.status(500).send({status : true , message: error.message})}
+
+    }
+}
+
+module.exports.createIntern=createIntern;
